a884dba5d8201dcf391c6bae4b7eddb0bd57b74c
xixeBombilla <tiliches_88@hotmail.com>
2012-05-26 10:25:10 -0500

    testins

--- test/unit/articulo_test.rb
+++ test/unit/articulo_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class ArticuloTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def primer_articulo
+    assert_not_nil(Articulo.first)
+  end
 end
